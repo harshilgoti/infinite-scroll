649561be56874d6f65d1fe0bc5b1fff4a79dc8bc
harshilgoti <51255488+harshilgoti@users.noreply.github.com>
2020-04-08 16:25:09 +0530

    react scroll

---
+++ src/Demo.js
@@ -0,0 +1,67 @@
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const style = {
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8
+};
+
+export default class Demo extends React.Component {
+  state = {
+    items: Array.from({ length: 20 }),
+    hasMore: true
+  };
+
+  fetchMoreData = () => {
+    if (this.state.items.length >= 500) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    // a fake async api call like which sends
+    // 20 more records in .5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 20 }))
+      });
+    }, 500);
+  };
+
+  render() {
+      console.log("state",this.state)
+    return (
+      <div style={{
+          height:"350px",
+          overflow:"auto",
+          width:"500px"
+         
+      }}
+      id="div"
+      >
+        <h1>react-infinite-scroll component</h1>
+        <hr />
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+          scrollableTarget={"div"}
+        >
+          
+          {this.state.items.map((i, index) => (
+            <div style={style} key={index}>
+              div - #{index}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+    );
+  }
+}
+
